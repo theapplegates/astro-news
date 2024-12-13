52f09c6b519bc7a7e030aa2741deae39ad3049aa
Paul Applegate <paul@theapplegates.com>
2024-12-12 19:08:21 -0500

    Forgot Quotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,15 @@ import pagefind from "astro-pagefind";
 
 import sitemap from '@astrojs/sitemap';
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: https://astro-news.paulapplegate.com,
-  base: /,
+  site: "https://astro-news.paulapplegate.com", // Enclosed in quotes
+  base: "/", // Enclosed in quotes
   markdown: {
     remarkPlugins: [readingTime, modifiedTime],
   },
   integrations: [tailwind(), mdx(), sitemap(), pagefind()],
   experimental: {
     responsiveImages: true,
-  }
+  },
 });
